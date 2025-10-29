c0e273f079707f0beb7b09fa92d5920861f00356
houssam largou <hossamlargo5@gmail.com>
2025-10-29 23:38:14 +0100

    add images for memory game cards

--- script.js
+++ script.js
@@ -1,30 +1,5 @@
-// let id = [];
-// document.querySelectorAll(".card").forEach((el) => {
-//   el.addEventListener("click", function () {
-//     this.querySelector(".front").classList.add("font-rotate");
-//     this.querySelector(".back").classList.add("back-rotate");
-//     const Num = this;
-//     for (let i = 0; i < id.length; i++) {
-//       if (+this.dataset.id == id[i]) {
-//         document.querySelectorAll(`card-${id[i]}`).forEach((el) => {
-//           this.querySelector(".front").classList.add("font-rotate");
-//           this.querySelector(".back").classList.add("back-rotate");
-//           id = [];
-//         });
-//       } else {
-//         setTimeout(function () {
-//           document.querySelectorAll(`card-${id[i]}`).forEach((el) => {
-//           Num.querySelector(".front").classList.remove("font-rotate");
-//           Num.querySelector(".back").classList.remove("back-rotate");
-//           id = [];
-//         }, 2000);
-//       }
-//     }
-//     id.push(+this.dataset.id);
-//   });
-// });
-
 let id = [];
+
 document.querySelectorAll(".card").forEach((el) => {
   el.addEventListener("click", function () {
     id.push(+this.dataset.id);
@@ -36,19 +11,7 @@ document.querySelectorAll(".card").forEach((el) => {
         if (id[i] == id[i + 1]) {
           id = [];
           return;
-        }
-        //  else if (id.length < 2) {
-        //   cacao.querySelector(".front").classList.remove("font-rotate");
-        //   cacao.querySelector(".back").classList.remove("back-rotate");
-        //   id = [];
-        // }
-        else {
-          //   cacao.querySelector(".front").classList.remove("font-rotate");
-          //   cacao.querySelector(".back").classList.remove("back-rotate");
-          //   querySelectorAll(`.card-${id[i]}`).forEach((el) => {
-          //     el.querySelector(".front").classList.remove("font-rotate");
-          //     el.querySelector(".back").classList.remove("font-rotate");
-          //   })
+        } else {
           document
             .querySelectorAll(`.card-${id[i]} , .card-${id[i + 1]}`)
             .forEach((el) => {
